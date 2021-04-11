d30e9bf1614cae8d27276d360a8f783c225403da
Altemush Bhatti <altemush@gmail.com>
2021-04-11 12:48:40 -0700

    Finished adding footer content and styling

--- benefits-concierge-dashboard/src/Footer/Footer.js
+++ benefits-concierge-dashboard/src/Footer/Footer.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => (
-    <Container>
-        <Row>
-            <Col>
-
+<div className='bcd-footer'>
+    <Container >
+        <Row className='justify-content-center'>
+            <Col  xl={8} className='bcd-footer-box'>
+                <span className='bcd-footer-item'>© Nayya Health, Inc. All rights reserved.</span>
+                <span className='bcd-footer-item'>Privacy Policy</span>
+                <span className='bcd-footer-item'>GDPR Notice</span>
+                <span className='bcd-footer-item'>Licenses</span>
             </Col>
         </Row>
     </Container>
+</div>
+    
 )
 
 export default Footer;
+
+
+
+
+
+
