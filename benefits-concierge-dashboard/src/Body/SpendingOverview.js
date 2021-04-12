c4f8ed88cfed60f278d738c1ff6e828955c587c7
Altemush Bhatti <altemush@gmail.com>
2021-04-11 19:46:46 -0700

    Made first overview tile component

--- benefits-concierge-dashboard/src/Body/SpendingOverview.js
+++ benefits-concierge-dashboard/src/Body/SpendingOverview.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import OverviewTile from './OverviewTile';
 
 const SpendingOverview = () => (
     <Container className='overview'>
         <Row>
-
+            <Col lg={12}>
+                <p className='overview-title'>My Spending Overview</p>
+            </Col>
         </Row>
+        <OverviewTile />
+            
+        
     </Container>
 )
 
