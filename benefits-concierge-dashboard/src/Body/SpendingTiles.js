af7534681972fdb189929e607ddf42a5033a6570
Altemush Bhatti <altemush@gmail.com>
2021-04-15 16:07:49 -0700

    Finished tiles

--- benefits-concierge-dashboard/src/Body/SpendingTiles.js
+++ benefits-concierge-dashboard/src/Body/SpendingTiles.js
@@ -11,11 +11,46 @@ const SpendingTiles = () => (
             savingsAmount={557}
             lifetimeSavings={127}
         />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
+        <Tile 
+            amountHeading='Save up to'
+            amount={`$${123}`}
+            firstName='Jeff'
+            info={[<span className='orange'>per year</span>, ' on your Tier I prescription drug Promethazine if you go to an alternate pharmacy nearby.']}
+            time={`${2} min`}
+            action='Save now ($)'
+        />
+        <Tile 
+            amountHeading='Save up to'
+            amount={`$${247}`}
+            firstName='Anna'
+            info={['by submitting your ',<span className='blue'>urgent care claim</span>, ' dated October 27, 2020 to Guardian.']}
+            time={`${9} min`}
+            action='Save now ($)'
+        />
+        <Tile 
+            amountHeading='Save up to'
+            amount={`$${123}`}
+            firstName='David'
+            info={[<span className='red'>per visit</span>, ' by changing to an in-network doctor for your recurring podiatry visits.']}
+            time={`${23} min`}
+            action='Save now ($)'
+        />
+        <Tile 
+            amountHeading='Dental cleanings'
+            amount={`${2}/yr`}
+            firstName='Jeff'
+            info={['Schedule your ',<span className='red'>covered semi-annual dental cleaning</span>, ' with a dentist nearest you. Its not too late.']}
+            time='David'
+            action='Find dentist'
+        />
+        <Tile 
+            amountHeading='Save up to'
+            amount={`$${23}`}
+            firstName='Jeff'
+            info={[<span className='orange'>per fill</span>, ' on your Tier I prescription drug Foradil if you go to an alternate pharmacy nearby.']}
+            time={`${3} days`}
+            action='Save now ($)'
+        />
     </div>
 )
 
--- benefits-concierge-dashboard/src/Body/Tile.js
+++ benefits-concierge-dashboard/src/Body/Tile.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Tile = () => (
-    <div></div>
+const Tile = ({amountHeading, amount, firstName, info, time, action}) => (
+    <Container>
+        <Row className='tile-row'>
+            <Col className='tile-icon' lg={1}>
+                <i>(i)</i>
+            </Col>
+            <Col lg={2}>
+                <div className='tile-heading'>{amountHeading}</div>
+                <span className='tile-amount'>{amount}</span>
+            </Col>
+            <Col lg={6}>
+                <div className='tile-firstName'>{firstName}</div>
+                <span className='tile-info'>{info}</span>
+            </Col>
+            <Col>
+                <div className='time'>{time}</div>
+                <button className='tile-action'>{action}</button>
+            </Col>
+        </Row>
+    </Container>
 )
 
 export default Tile;
